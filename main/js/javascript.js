097295e34b3b863cdcfcd56488cbac37bf2f3217
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-21 09:53:04 -0400

    section bar stablished

--- main/js/javascript.js
+++ main/js/javascript.js
@@ -1,7 +1,5 @@
 var pictures = ["main/img/a1.jpg", "main/img/a2.jpg", "main/img/a3.jpg", "main/img/a4.jpg"]
 
-
-
 pictures.forEach(function(el, i) {
     var container = document.getElementById('pictures'); //Search for the id where i want to put my pictures
     var crearDiv = document.createElement('div'); //create the div element
